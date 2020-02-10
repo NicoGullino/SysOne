335caf9b4a32677dd592191f900fa03cfbc61e9c
NicoGullino <43994767+NicoGullino@users.noreply.github.com>
2020-02-10 09:41:39 -0300

    Costos

---
+++ src/main/java/com/sysone/costos/CostosApplication.java
@@ -0,0 +1,12 @@
+package com.sysone.costos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostosApplication.class, args);
+	}
+}
